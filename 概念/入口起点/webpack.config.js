c95759fadd13cd5bd4024391022b2e22d790375b
李子豪 <15836784857@163.com>
2019-09-18 16:54:47 +0800

    vue

---
+++ 概念/入口起点/webpack.config.js
@@ -0,0 +1,11 @@
+let path = require("path");
+
+module.exports = {
+    mode: "development",
+    // entry: "./src/index.js",
+    // entry: ["./src/index.js", "./src/test.js"],
+    entry: {
+        index: "./src/index.js",
+        test: "./src/test.js",
+    }
+};
